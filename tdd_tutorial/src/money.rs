0d0077f661416578edb653533067dd5baa431b7e
aochan <nakinagaraodoru1013@gmail.com>
2024-10-16 09:30:09 +0000

    close #10

--- tdd_tutorial/src/money.rs
+++ tdd_tutorial/src/money.rs
@@ -13,9 +13,24 @@ impl Dollar {
     }
 }
 
+#[allow(dead_code)]
+#[derive(PartialEq, Eq, Debug)]
+struct Franc {
+    amount: u8,
+}
+
+impl Franc {
+    #[allow(dead_code)]
+    fn times(&self, multiplier: u8) -> Franc {
+        return Self {
+            amount: self.amount * multiplier,
+        };
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::money::Dollar;
+    use crate::money::{Dollar, Franc};
     #[test]
     fn multiplication() {
         let five = Dollar { amount: 5 };
@@ -27,5 +42,13 @@ mod tests {
     #[test]
     fn equality() {
         assert_eq!(Dollar { amount: 5 }, Dollar { amount: 5 });
+    }
+
+    #[test]
+    fn franc_multiplication() {
+        let five = Franc { amount: 5 };
+
+        assert_eq!(Franc { amount: 10 }, five.times(2));
+        assert_eq!(Franc { amount: 15 }, five.times(3));
     }
 }
